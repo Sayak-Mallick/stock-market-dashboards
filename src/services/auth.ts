8f5193ce440bd66acf2954a415fdd778a0f76771
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-07-07 09:48:26 +0530

    Option Greek Calculator completed

--- src/services/auth.ts
+++
@@ -1,5 +0,0 @@
-export const redirectToLogin = async () => {
-  const res = await fetch("http://localhost:5000/login-url");
-  const data = await res.json();
-  window.location.href = data.url;
-};
--- src/services/getWsUrl.ts
+++
@@ -1,6 +0,0 @@
-export const fetchWsUrl = async (token: string) => {
-  const res = await fetch(`http://localhost:5000/ws-url?token=${token}`);
-
-  const data = await res.json();
-  return data.data.authorized_redirect_uri;
-};
